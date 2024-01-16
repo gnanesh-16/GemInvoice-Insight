866d1e06fb2b8e0cffea3ed24bc1997df480c3f2
Gnanesh Balusa <98212179+gnanesh-16@users.noreply.github.com>
2024-01-16 11:04:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,9 +42,9 @@ if uploaded_file is not None:
 submit=st.button("Tell me about the invoice")
 
 input_prompt="""
-You are an expert in understanding invoices. We will upload a a image as invoice
+You are an expert in understanding invoices. We will upload a image as invoice
 and you will have to answer any questions based on the uploaded invoice image 
-and aslo you can give it in Table formate of invoice Details.
+and also you can give it in Table formate of invoice Details.
 """
 
 ## if submit button is clicked
